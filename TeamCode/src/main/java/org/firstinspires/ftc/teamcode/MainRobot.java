99f7f8073c73913373145e0b699f066db079f82c
AnishJag <jagannathananish@gmail.com>
2021-01-02 18:38:48 -0600

    Added GyroDrive and started blue autonomous.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainRobot.java
@@ -2,12 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.SensorMRRangeSensor;
 
@@ -34,8 +37,10 @@ public class MainRobot {
     static final double     COUNTS_PER_MOTOR_REV    = 386.3;
     static final double     DRIVE_GEAR_REDUCTION    = 1.0;
     static final double     WHEEL_DIAMETER_INCHES   = 3.77953;
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION)
+    public static final double     COUNTS_PER_INCH  = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION)
                                                              / (WHEEL_DIAMETER_INCHES * 3.14159265);
+    public static final double     DRIVE_SPEED      = 1.0;
+    public static final double     TURN_SPEED       = 0.5;
 
     HardwareMap hwMap = null;
 
@@ -77,5 +82,133 @@ public class MainRobot {
         gyro = hwMap.get(ModernRoboticsI2cGyro.class,"gyro");
         gyro.initialize();
         gyro.calibrate();
+    }
+
+    public double getError (double angle){
+
+        double robotError;
+
+        // calculate error in -179 to +180 range
+        robotError = angle - gyro.getIntegratedZValue();
+        while (robotError > 180)  robotError -= 360;
+        while (robotError <= -180) robotError += 360;
+        return robotError;
+    }
+
+    public double getSteer (double error, double P_DRIVE_COEFF){
+        return Range.clip(error * P_DRIVE_COEFF, -1, 1);
+    }
+
+    //Autonomous Blue & Red
+    public void gyroDrive (double speed, double distanceTL, double distanceTR,
+                           double distanceBL, double distanceBR, double angle, LinearOpMode opmode) {
+
+        int     newTLTarget;
+        int     newTRTarget;
+        int     newBLTarget;
+        int     newBRTarget;
+        int     moveCountsTL = (int)(distanceTL * MainRobot.COUNTS_PER_INCH);
+        int     moveCountsTR = (int)(distanceTR * MainRobot.COUNTS_PER_INCH);
+        int     moveCountsBL = (int)(distanceBL * MainRobot.COUNTS_PER_INCH);
+        int     moveCountsBR = (int)(distanceBR * MainRobot.COUNTS_PER_INCH);
+        double  max;
+        double  leftMax;
+        double  rightMax;
+        double  error;
+        double  speedTL;
+        double  speedTR;
+        double  speedBL;
+        double  speedBR;
+
+        // Ensure that the Op-mode is still active
+        if (opmode.opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newTLTarget  = topLeft.getCurrentPosition() + moveCountsTL;
+            newTRTarget  = topRight.getCurrentPosition() + moveCountsTR;
+            newBLTarget  = bottomLeft.getCurrentPosition() + moveCountsBL;
+            newBRTarget  = bottomRight.getCurrentPosition() + moveCountsBR;
+
+            // Set Target and Turn On RUN_TO_POSITION
+            topLeft.setTargetPosition(newTLTarget);
+            topRight.setTargetPosition(newTRTarget);
+            bottomLeft.setTargetPosition(newBLTarget);
+            bottomRight.setTargetPosition(newBRTarget);
+
+            topLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            topRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            bottomLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            bottomRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // start motion
+            speed = Range.clip(Math.abs(speed), 0.0, 1.0);
+            topLeft.setPower(speed);
+            topRight.setPower(speed);
+            bottomLeft.setPower(speed);
+            bottomRight.setPower(speed);
+
+            // keep looping while we are still active, and BOTH motors are running.
+            while (opmode.opModeIsActive() &&
+                    (topLeft.isBusy() && topRight.isBusy() && bottomLeft.isBusy() && bottomRight.isBusy())) {
+
+                // adjust relative speed based on heading error.
+                error = getError(angle);
+                double steer = getSteer(error, 0.15);
+
+                // if driving in reverse, the motor correction also needs to be reversed
+                if (distanceTL > 0)
+                    speedTL  = speed - steer;
+                else speedTL = speed + steer;
+
+                if (distanceTR > 0)
+                    speedTR  = speed + steer;
+                else speedTR = speed - steer;
+
+                if (distanceBL > 0)
+                    speedBL  = speed - steer;
+                else speedBL = speed + steer;
+
+                if (distanceBR > 0)
+                    speedBR  = speed + steer;
+                else speedBR = speed - steer;
+
+                // Normalize speeds if either one exceeds +/- 1.0;
+                leftMax  = Math.max(Math.abs(speedTL), Math.abs(speedTR));
+                rightMax = Math.max(Math.abs(speedBL), Math.abs(speedBR));
+                max      = Math.max(leftMax,rightMax);
+                if (max > 1.0)
+                {
+                    speedTL /= max;
+                    speedTR /= max;
+                    speedBL /= max;
+                    speedBR /= max;
+                }
+
+                topLeft.setPower(speedTL);
+                topRight.setPower(speedTR);
+                bottomLeft.setPower(speedBL);
+                bottomRight.setPower(speedBR);
+
+                // Display drive status for the driver.
+                opmode.telemetry.addData("Err/St",  "%5.1f/%5.1f",  error, steer);
+                opmode.telemetry.addData("Target",  "%7d:%7d:%7d:%7d",      newTLTarget, newTRTarget, newBLTarget, newBRTarget);
+                opmode.telemetry.addData("Actual",  "%7d:%7d:%7d:%7d",      topLeft.getCurrentPosition(),
+                        topRight.getCurrentPosition(), bottomLeft.getCurrentPosition(), bottomRight.getCurrentPosition() );
+                opmode.telemetry.addData("Speed",   "%5.2f:%5.2f:%5.2f:%5.2f",  speedTL, speedTR, speedBL, speedBR);
+                opmode.telemetry.update();
+            }
+
+            // Stop all motion;
+            topLeft.setPower(0);
+            topRight.setPower(0);
+            bottomLeft.setPower(0);
+            bottomRight.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            topLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            topRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            bottomLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            bottomRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/Blue.java
@@ -3,20 +3,50 @@ package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.MainRobot;
+
 
 @Autonomous(name="Blue")
-public class
-Blue extends LinearOpMode {
+public class Blue extends LinearOpMode {
+
+    MainRobot robot = new MainRobot();
 
     @Override
     public void runOpMode() throws InterruptedException {
+
         RingDetector detector = new RingDetector(this, false);
 
+        robot.init(hardwareMap);
+
+        robot.topLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.topRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.bottomLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.bottomRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.topLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.topRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.bottomLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.bottomRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
         waitForStart();
+
+        robot.gyro.resetZAxisIntegrator();
 
         int rings = detector.getDecision();
 
         if (rings == 0){
+            robot.gyroDrive(robot.DRIVE_SPEED,3,3,3,3,0,this);
+
+            robot.wobbleArm.setPower(1);
+            sleep(1000);
+            robot.wobbleArm.setPower(-1);
+            sleep(1000);
+            robot.wobbleArm.setPower(0);
+
 
         }
         /*
@@ -25,8 +55,8 @@ Blue extends LinearOpMode {
         3. Robot deposits second wobble goal into position A.
         4. Robot trails behind white line, aims, and shoots for three power-shots.
         5. After completion of steps 1-4, robot parks on white line.
-        This process is the same for "if (rings == 1)".
+        This process is the same for "if (rings == 1).
        */
-
     }
+
 }
